59cba8a2fee87ca02a44f261b4c9fcf7ec619c1c
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 12:39:18 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,19 @@
 #KANGERS_GIVE_CREDITS
+import os
+from pyrogram import filters
+from main import bot
+
+START_TEXT = """
+Hi <b>{}</b> 👋
+I can play music & stream videos in Telegram group voice chats. 
+Make your own bot using below source code.
+"""
+
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("📨 Support", url="https://t.me/TITANIUM_XYZ"),
+            InlineKeyboardButton("📚 Source Code", url="https://github.com/TitaniumOp/MusicBot")
+        ]
+    ]
+)
