64dbb95d023bc9eb0eac617a27292a9f70834660
darkophacker0987 <82193878+darkophacker0987@users.noreply.github.com>
2022-05-29 08:56:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ load_dotenv()
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "1829202"))
-API_ID = int(getenv("API_ID", "8186557")) #oP
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
+OWNER_ID = int(getenv("OWNER_ID", "5369003337"))
+API_ID = int(getenv("API_ID", "3706077")) #oP
+API_HASH = getenv("API_HASH", "5bec73923f996b3092a4209ac556bc58")
